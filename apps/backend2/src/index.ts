afad755ecc828855ee3c61bd2eddfef9304b51e0
p1yu5h0 <piyush200205@gmail.com>
2024-03-18 17:34:08 +0530

    updated common

--- apps/backend2/src/index.ts
+++ apps/backend2/src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
-// import NUMBER from "@repo/common";
+import {NUMBER, add, subtract} from "@repo/common/config";
 
 const app = express()
 
 app.get("/", (req, res) => {
     console.log('port 3003 (backend 2) hit by server')
+    const c = add(4, 5);
     res.json({
         message: "hello world",
-        // number: NUMBER
+        number: NUMBER,
+        c: c,
     });
 })
 
